6efb4df4099d73eda4338cbb632bc44097409d4d
xingnliu <xingnliu@10.140.44.130>
2019-09-04 15:12:56 +0800

    Change to a multi-module project

---
+++ appdemo-auth-service/src/main/java/org/appdemo/auth/service/App.java
@@ -0,0 +1,13 @@
+package org.appdemo.auth.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
