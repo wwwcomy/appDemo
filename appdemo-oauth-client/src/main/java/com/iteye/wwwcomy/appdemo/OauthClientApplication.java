169d882f85fea831cee6c6cfdb738461d6fda82e
wwwcomy <wwwcomy@gmail.com>
2019-09-10 17:43:32 +0800

    Let's have yaml file...

---
+++ appdemo-oauth-client/src/main/java/com/iteye/wwwcomy/appdemo/OauthClientApplication.java
@@ -0,0 +1,13 @@
+package com.iteye.wwwcomy.appdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthClientApplication.class, args);
+	}
+
+}
