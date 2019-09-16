797a22bbc489526334f38ad0474ab3648629ec93
wwwcomy <wwwcomy@gmail.com>
2019-09-16 17:58:22 +0800

    Add some configuration but not tested yet. Will use this client project with existing OAuth2 server for a test

--- appdemo-oauth-client/src/main/java/com/iteye/wwwcomy/appdemo/OauthClientApplication.java
+++ appdemo-oauth-client/src/main/java/com/iteye/wwwcomy/appdemo/OauthClientApplication.java
@@ -2,12 +2,24 @@ package com.iteye.wwwcomy.appdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class OauthClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OauthClientApplication.class, args);
+	}
+
+	public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.antMatcher("/**").authorizeRequests().antMatchers("/", "/js/**", "/login", "/index.html").permitAll().anyRequest()
+					.authenticated().and().logout().logoutSuccessUrl("/").permitAll().and().csrf().disable();
+		}
 	}
 
 }
