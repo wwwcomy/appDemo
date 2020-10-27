cd8d9bb74fe7ca8b27d92e3cd4e107a039ac5b2c
wwwcomy <wwwcomy@gmail.com>
2020-10-27 14:19:17 +0800

    expose token key endpoint

--- appdemo-auth-service/src/main/java/com/iteye/wwwcomy/appdemo/AuthService.java
+++ appdemo-auth-service/src/main/java/com/iteye/wwwcomy/appdemo/AuthService.java
@@ -1,17 +1,12 @@
 package com.iteye.wwwcomy.appdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,16 +14,9 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.approval.ApprovalStore;
-import org.springframework.security.oauth2.provider.approval.ApprovalStoreUserApprovalHandler;
-import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
-import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
-import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -87,26 +75,6 @@ class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 }
 
 @Configuration
-class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-
-	@Override
-	public void configure(ResourceServerSecurityConfigurer resources) {
-		resources.resourceId("USERS");
-	}
-
-	@Override
-	public void configure(HttpSecurity http) throws Exception {
-		http
-		// Since we want the protected resources to be accessible in the UI as well we
-		// need
-//				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and()
-				.requestMatchers().antMatchers("/api/**").and().authorizeRequests()
-//                .antMatchers("/product/**").access("#oauth2.hasScope('select') and hasRole('ROLE_USER')")
-				.anyRequest().authenticated();
-	}
-}
-
-@Configuration
 class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Override
@@ -155,12 +123,13 @@ class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-		oauthServer.allowFormAuthenticationForClients();
+		oauthServer.allowFormAuthenticationForClients().tokenKeyAccess("permitAll()").checkTokenAccess("permitAll()");
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.tokenServices(tokenServices()).tokenStore(tokenStore())
+		endpoints.tokenServices(tokenServices())
+		// .tokenStore(tokenStore())
 //		.authenticationManager(authenticationManager)
 		;
 	}
